ff974cfde8870f19b1261a61823b59d8705da5b6
Nicolas  Muñoz <44203419+Nicolasmp92@users.noreply.github.com>
2025-03-06 15:41:59 +0000

    commit desde codespace: cambiando icono app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cafe';
+
+  toggleTheme() {
+    const body = document.body;
+    body.classList.toggle('light-mode');
+  
+    // Cambiar icono según el modo
+    const btn = document.querySelector('.theme-toggle-btn i');
+    if (body.classList.contains('light-mode')) {
+      btn?.classList.replace('bi-moon', 'bi-sun'); // Icono de sol
+    } else {
+      btn?.classList.replace('bi-sun', 'bi-moon'); // Icono de luna
+    }
+  }
+  
 }
