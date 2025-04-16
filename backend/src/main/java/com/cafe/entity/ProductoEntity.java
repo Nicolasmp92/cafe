26e6268f21019334af5b85f862719551f31a0426
Víctor Bastian <abana343@gmail.com>
2025-04-16 15:46:57 -0400

    se crea el producto DB

---
+++ backend/src/main/java/com/cafe/entity/ProductoEntity.java
@@ -0,0 +1,49 @@
+package com.cafe.entity;
+
+
+import com.cafe.dto.Categoria;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Product", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "name")
+})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductoEntity {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Categoria category;
+
+}
