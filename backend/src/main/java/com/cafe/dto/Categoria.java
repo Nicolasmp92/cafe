2e9b8d088109c6ebec7426fc43e6161824a616a1
Nicolas Muñoz <nikolasmp92@gmail.com>
2025-05-10 12:52:33 -0400

    eliminando mi back para reemplasarlo con el de bastian

--- backend/src/main/java/com/cafe/dto/Categoria.java
+++
@@ -1,9 +0,0 @@
-package com.cafe.dto;
-
-public enum Categoria {
-
-	COFFEE,
-    TEA,
-    SNACK,
-    OTHER
-}
--- backend/src/main/java/com/cafe/repositoy/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cafe.repositoy;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cafe.entity.ProductoEntity;
-
-public interface ProductoRepository extends JpaRepository<ProductoEntity, Long> {
-
-}
